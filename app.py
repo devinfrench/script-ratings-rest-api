ac736f63ba1b337f6bf46050f451f5612081b93f
Devin <devinfrench@users.noreply.github.com>
2019-01-12 13:33:43 -0800

    Add routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os
@@ -29,6 +29,47 @@ class Rating(db.Model):
 
 rating_schema = RatingSchema(strict=True)
 ratings_schema = RatingSchema(many=True, strict=True)
+
+
+@app.route("/ratings", methods=["GET"])
+def get_all_ratings():
+    ratings = Rating.query.all()
+    result = ratings_schema.dump(ratings)
+    return jsonify(result.data)
+
+
+@app.route("/ratings/<script>", methods=["GET"])
+def get_ratings(script):
+    ratings = Rating.query.filter_by(script=script).all()
+    result = ratings_schema.dump(ratings)
+    return jsonify(result.data)
+
+
+@app.route("/rating", methods=["POST"])
+def create_rating():
+    rating = request.json["rating"]
+    script = request.json["script"]
+    username = request.json["username"]
+    new_rating = Rating(rating, script, username)
+    db.session.add(new_rating)
+    db.session.commit()
+    return rating_schema.jsonify(new_rating)
+
+
+@app.route("/rating", methods=["PUT"])
+def update_rating():
+    rating = Rating.query.filter_by(script=request.json["script"], username=request.json["username"]).first()
+    rating.rating = request.json["rating"]
+    sb.session.commit()
+    return rating_schema.jsonify(rating)
+
+
+@app.route("/rating", methods=["DELETE"])
+def delete_rating():
+    rating = Rating.query.filter_by(script=request.json["script"], username=request.json["username"]).first()
+    db.session.delete(rating)
+    db.session.commit()
+    return rating_schema.jsonify(rating)
 
 
 if __name__ == "__main__":
