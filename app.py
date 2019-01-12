67057f725b729000d6793259844de28911944eeb
Devin <devinfrench@users.noreply.github.com>
2019-01-12 12:55:16 -0800

    Add rating model

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    script = db.Column(db.String(50))
+    username = db.Column(db.String(255))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
